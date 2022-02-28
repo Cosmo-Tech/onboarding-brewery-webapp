688a467a62b4691a09998646a70b030c9a3056f9
jplanchenault <jean-charles.planchenault@cosmotech.com>
2022-02-28 12:05:43 +0100

    Deploy web app for dry run on thecosmocompany69

--- src/config/AppInstance.js
+++ src/config/AppInstance.js
@@ -2,17 +2,17 @@
 // Licensed under the MIT license.
 
 // Azure App registration details
-export const AZURE_TENANT_ID = 'e413b834-8be8-4822-a370-be619545cb49';
-export const APP_REGISTRATION_CLIENT_ID = 'd104fbd6-9464-45d5-a022-83c90ad56906';
+export const AZURE_TENANT_ID = 'e9641c78-d0d6-4d09-af63-168922724e7f';
+export const APP_REGISTRATION_CLIENT_ID = '0d2104f8-4b42-43dc-ae89-df7f5556997e';
 export const APPLICATION_INSIGHTS_INSTRUMENTATION_KEY = 'f198a852-15d8-48da-9cd0-1e6d18708489';
 
 // API information
-export const COSMOTECH_API_SCOPE = 'http://dev.api.cosmotech.com/platform';
-export const DEFAULT_BASE_PATH = 'https://dev.api.cosmotech.com';
+export const COSMOTECH_API_SCOPE = 'https://dryrun.api.thecosmocompany69.onmicrosoft.com/platform';
+export const DEFAULT_BASE_PATH = 'https://dryrun.test.api.cosmo-platform.com';
 
 // Cosmo solution identifiers
-export const ORGANIZATION_ID = 'O-gZYpnd27G7';
-export const WORKSPACE_ID = 'W-rXeBwRa0PM';
+export const ORGANIZATION_ID = 'o-p307vpw960dd';
+export const WORKSPACE_ID = 'w-9pjr40gl055k';
 
 // Power BI information
 export const POWER_BI_WORKSPACE_ID = '290de699-9026-42c0-8c83-e4e87c3f22dd';
